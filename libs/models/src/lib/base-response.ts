9bc3290a977201f2e6a9336b51f51629ac288216
Alessandro-Incantalupo <alessandro.incantalupo@gmail.com>
2025-10-04 21:47:38 +0200

    fix: pushed comments to deploy docker image. added feature to update docker image when pull request are approved on main

--- libs/models/src/lib/base-response.ts
+++ libs/models/src/lib/base-response.ts
@@ -1,9 +1,11 @@
+// BaseResponse is a generic type that represents a standard API response structure.
 export type BaseResponse<T = unknown> = {
   data: T;
   message: string | null;
   code: number | null;
 };
 
+// PaginatedResponse extends BaseResponse to include pagination metadata.
 export type PaginatedResponse<T> = BaseResponse<T[]> & {
   meta: {
     totalItems: number;
